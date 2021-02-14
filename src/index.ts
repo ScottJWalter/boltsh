2ca606a341c57d127e3a1d98dc12c60a16230dd9
Scott Walter <sjwalter@gmail.com>
2021-02-14 12:41:00 -0600

    refactored and fixed ascii-table type def

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { Command, flags } from '@oclif/command'
+import {Command, flags} from '@oclif/command'
 import * as neo4j from 'neo4j-driver'
-import * as AsciiTable from 'ascii-table'
+
+const AsciiTable = require('ascii-table')
 
 class Boltsh extends Command {
   static description = 'Execute Cypher Queries via Bolt'
@@ -15,29 +16,29 @@ Tom Hanks
   ]
 
   static flags = {
-    version: flags.version({ char: 'v' }),
+    version: flags.version({char: 'v'}),
     // add --help flag to show CLI version
-    help: flags.help({ char: 'h' }),
+    help: flags.help({char: 'h'}),
 
-    address: flags.string({ char: 'a', description: 'bolt address', required: true, default: 'bolt://localhost' }),
-    user: flags.string({ char: 'u', description: 'neo4j user', required: true, default: 'neo4j' }),
-    password: flags.string({ char: 'p', description: 'user password', required: true }),
-    table: flags.boolean({ char: 't', description: 'Table Format' })
+    address: flags.string({char: 'a', description: 'bolt address', required: true, default: 'bolt://localhost'}),
+    user: flags.string({char: 'u', description: 'neo4j user', required: true, default: 'neo4j'}),
+    password: flags.string({char: 'p', description: 'user password', required: true}),
+    table: flags.boolean({char: 't', description: 'Table Format'})
   }
 
-  static args = [{ name: 'query', required: true, description: 'Cypher Query to Run' }]
+  static args = [{name: 'query', required: true, description: 'Cypher Query to Run'}]
 
   async run() {
-    const { args, flags } = this.parse(Boltsh)
+    const {args, flags} = this.parse(Boltsh)
 
     // this.log(`hello ${flags.address} ${flags.user} ${args.query} from ${__filename}!`)
 
-    const driver = neo4j.v1.driver(flags.address, neo4j.v1.auth.basic(flags.user, flags.password))
+    const driver = neo4j.driver(flags.address, neo4j.auth.basic(flags.user, flags.password))
     const session = driver.session()
     const result = await session.run(args.query)
     session.close()
     const records = result.records;
-    const data = { heading: records[0].keys, rows: records.map(r => r.keys.map(k => r.get(k))) }
+    const data = {heading: records[0].keys, rows: records.map(r => r.keys.map(k => r.get(k)))}
     if (flags.table) {
       const table = AsciiTable.factory(data)
       this.log(table.toString())
